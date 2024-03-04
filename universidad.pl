854c9e6d9f14bec176283453714408c7d6efbdbf
lpze.marielalejandra.huayta.qu@unifranz.edu.bo <lpze.marielalejandra.huayta.qu@unifranz.edu.bo>
2024-03-04 00:19:51 -0400

    tarea2

---
+++ universidad.pl
@@ -0,0 +1,32 @@
+% Átomos
+cristiano.
+mariel.
+juan.
+batista.
+cristal.
+mario.
+alejandra.
+adrian.
+luciano.
+alvaro.
+arturica.
+andres.
+carla.
+magenta.
+giovanna.
+elena.
+
+% Condiciones
+esEstudiante(cristiano).
+esEstudiante(mariel).
+esEstudiante(juan).
+esEstudiante(batista).
+esEstudiante(cristal).
+esEstudiante(mario).
+esEstudiante(alejandra).
+esEstudiante(adrian).
+esEstudiante(luciano).
+esEstudiante(alvaro).
+esDocente(mario).
+esDocente(arturica).
+esDocente(andres).
